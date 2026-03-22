331d0c1870d91266c2d761c1a9c0c89c794cb9b5
Kelandra Pringle <kelandrapringle@gmail.com>
2026-03-21 21:49:59 -0500

    Updated files for Lab 5

--- src/views/MoviesView.vue
+++ src/views/MoviesView.vue
@@ -19,36 +19,36 @@ onMounted(() => {
 </script>
 
 <template>
-    <div class="container mt-4">
-      <h2>Movies</h2>
-      <div class="row row-cols-1 row-cols-md-2 g-3">
-        <div
-          class="col"
-          v-for="movie in movies"
-          :key="movie.id"
-        >
-          <div class="card h-100">
-            <div class="row g-0 h-100">
-              <div class="col-4" style="overflow: hidden;">
-                <img
-                  :src="movie.poster"
-                  :alt="movie.title"
-                  style="width: 100%; height: 100%; object-fit: cover; object-position: top;"
-                />
-              </div>
-              <div class="col-8">
-                <div class="card-body">
-                  <h5 class="card-title">{{ movie.title }}</h5>
-                  <p class="card-text" style="font-size: 0.85rem;">{{ movie.description }}</p>
-                </div>
+  <div class="container mt-4">
+    <h2>Movies</h2>
+    <div class="row row-cols-1 row-cols-md-2 g-3">
+      <div
+        class="col"
+        v-for="movie in movies"
+        :key="movie.id"
+      >
+        <div class="card h-100">
+          <div class="row g-0">
+            <div class="col-4">
+              <img
+                :src="movie.poster"
+                :alt="movie.title"
+                style="width: 100%; height: 180px; object-fit: cover; object-position: center;"
+              />
+            </div>
+            <div class="col-8">
+              <div class="card-body">
+                <h5 class="card-title">{{ movie.title }}</h5>
+                <p class="card-text" style="font-size: 0.85rem;">{{ movie.description }}</p>
               </div>
             </div>
           </div>
         </div>
       </div>
-      <p v-if="movies.length === 0">No movies added yet.</p>
     </div>
-  </template>
+    <p v-if="movies.length === 0">No movies added yet.</p>
+  </div>
+</template>
 
 <style>
 </style>
